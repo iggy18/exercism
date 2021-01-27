329b18c9dbf764b128a286a3a29d21853303884c
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2021-01-26 18:20:00 -0800

    set up arrays for 12 days problem

---
+++ twelve-days/twelve_days.py
@@ -0,0 +1,6 @@
+def recite(start_verse, end_verse):
+    gifts = ['a Partridge in a Pear Tree.','and a Partridge in a Pear Tree.', 'two Turtle Doves, ', 'three French Hens, ', 'four Calling Birds, ', 'five Gold Rings, ', 'six Geese-a-Laying, ', 'seven Swans-a-Swimming, ', 'eight Maids-a-Milking, ', 'nine Ladies Dancing, ', 'ten Lords-a-Leaping, ', 'eleven Pipers Piping, ', 'twelve Drummers Drumming, ']
+    number = [1,2,3,4,5,6,7,8,9,10,11,12]
+    word_number = ['first', 'second', 'third', 'fourth', 'fith', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth', 'eleventh', 'tweflth']
+
+    return f'"On the first day of Christmas my true love gave to me: '
