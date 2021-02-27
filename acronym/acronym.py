1db414d352e0f6c0de94ded640aae43fe1067931
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2021-02-26 19:44:48 -0800

    regex not getting dashes or underlines

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,4 +1,11 @@
-def abbreviate(words):
+import re
 
-    x = words.split()
+def abbreviate(words):
+    clean = [re.sub(r"[^a-zA-Z0-9]+", ' ', k) for k in words.split()]
+    result = []
+    for word in clean:
+        if word == " ":
+            word = ""
+        result.append(word[0])
+    return "".join(result).upper()
     
